1ecb57d01c23d87343911a518487746d3c430703
Mujtaba662 <83132110+Mujtaba662@users.noreply.github.com>
2022-08-15 02:39:48 +0400

    Made the code better

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 let count = 0
 
 function countUP() {
-    count = count + 1
+    count += 1
     let counter = document.getElementById('counter')
     counter.innerText = count
 }
 function countDOWN() {
-    count = count -1 
+    count -= 1 
     let counter = document.getElementById('counter')
     counter.innerText = count
 }
