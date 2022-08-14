46426b868ba5f6a2fc76c10f638644fdee4f5ec2
Mujtaba662 <83132110+Mujtaba662@users.noreply.github.com>
2022-08-15 02:38:26 +0400

    Final counter changes
    Added new buttons and stuff

--- index.js
+++ index.js
@@ -15,3 +15,15 @@ function deleteALL() {
     let counter = document.getElementById('counter')
     counter.innerText = count
 }
+function saveCOUNT() {
+    count = count 
+    let save = document.getElementById('save')
+    save.innerText += " " + count
+    count = 0
+    let counter = document.getElementById('counter')
+    counter.innerText = 0
+}
+function deleteENTRIES() {
+    let save = document.getElementById('save')
+    save.innerText = "Previous enteries: "
+}
